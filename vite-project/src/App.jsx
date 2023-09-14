1b330b281d0cf6bfb3a849f22cd30108a1d97a1d
Jake Moretz <jmoretz9090@gmail.com>
2023-09-14 16:31:08 -0400

    init set up

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css"
+import Die from './components/Die'
+
+export default function App() {
+    return (
+        
+        <main className='main-container'>
+            <section className="die-container">
+                <Die />
+                <Die />
+                <Die />
+                <Die />
+                <Die />
+                <Die />
+                <Die />
+                <Die />
+                <Die />
+                <Die />
+            </section>
+        </main>
+    )
+}
---
+++ vite-project/src/components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die() {
+    return (
+        <section className = "die">  
+                1   
+        </section>
+    )
+}
